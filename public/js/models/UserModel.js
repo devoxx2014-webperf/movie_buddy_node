fb1f20aab2f8be377c73d8df2cd4fb3d74eb2376
Philippe CHARRIERE <ph.charriere@gmail.com>
2014-03-30 19:17:18 +0200

    Update UserModel.js

--- public/js/models/UserModel.js
+++ public/js/models/UserModel.js
@@ -1,12 +1,12 @@
 /*--- User Model ---*/
 
 var UserModel = Backbone.Model.extend({
-    defaults : function (){
-      return {
-        name : "John Doe"
-      }
-    },
-    idAttribute: "_id",
-    urlRoot : "users"
+  defaults : function (){
+    return {
+      name : "John Doe"
+    }
+  },
+  idAttribute: "_id",
+  urlRoot : "users"
 });
 
